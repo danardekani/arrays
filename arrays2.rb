898e8d68417dcdc06c5b799ff24a51d0a43ea710
Daniel <ardekanicapital@gmail.com>
2019-06-24 22:57:52 -0400

    practice arrays

---
+++ arrays2.rb
@@ -0,0 +1,78 @@
+# 1. Write a times loop to print the numbers 1 through 10.
+
+number = 1 
+10.times do 
+  p number
+  number += 1
+end
+
+# 2. Write a times loop that prints the word "hello" 5 times.
+
+word = "hello"
+5.times do
+  p word
+end
+
+# 3. Write a times loop that prints the numbers 10 through 1.
+
+n = 10
+10.times do
+  p n
+  n -= 1
+end
+
+
+# 4. Write a times loop that prints the numbers 0 through 15, increasing by 5 each time.
+
+n = 0 
+4.times do
+  p n
+  n += 5
+end
+
+
+# 5. Write a times loop that prints the number 9000 ten times.
+
+10.times do
+  p 9000
+end
+
+
+# 6. Write a times loop that prints the numbers 100 through 90.
+
+n = 100
+11.times do
+  p n
+  n -= 1
+end
+
+
+# 7. Write a times loop that prints the numbers 50 to 70.
+
+n = 50 
+21.times do 
+  p n
+  n += 1
+end
+
+# 8. Write a times loop that prints the phrase "Around the world" 144 times.
+
+144.times do
+  p "Around the world"
+end
+
+# 9. Write a times loop that prints the numbers 5 through 0.
+
+n = 5 
+6.times do
+  p n 
+  n -= 1
+end
+
+10. Write a times loop that prints the even numbers from 2 to 10.
+
+n = 2
+5.times do 
+  p n 
+  n += 2
+end
